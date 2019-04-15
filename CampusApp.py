b08641996d435b179dc6b889ae7c2ac856fccb13
zippi100 <33308432+zippi100@users.noreply.github.com>
2019-04-15 01:26:21 -0700

    Testing input and outputing to terminal

--- CampusApp.py
+++ CampusApp.py
@@ -5,10 +5,58 @@ import os
 
 screen = tk.Tk()
 
+
 def search():
+	floors_file = open("Floors.txt", "r")
+	flag = 0
+	start = 0
+	searched = entry_box.get()
+	last_position = floors_file.tell()
 	
-	searched = str(entry_box.get())
-	print(searched)
+	# print (floors_file.readline())
+	"""for line in floors_file.readline():
+		print ("test 1")
+		print (line)
+		for word in line.split():
+			print ("test 2")
+			print (word)
+			if word == searched:
+				print ("test 3")
+				flag = 1
+				while word != ',':
+	
+				print (line)"""\
+	
+	if searched == '':
+		pass
+	
+	#elif int(searched) >= 0 or int(searched) < 0:
+	#	pass
+	
+	elif searched in floors_file.read():
+		
+		floors_file.seek(last_position)		
+		
+		for line in floors_file.readlines():
+			
+			if searched in line:			
+				start = 1
+
+			if start == 0:
+				pass
+			else:
+				if "," in line:				
+					break
+				else:
+					print (line)
+		flag = 1
+	
+	if flag == 0:
+		not_valid = tk.Label(screen, text="Not A Valid Input, Search Again")
+		not_valid.place(x=625, y=25)
+		print("Not a valid input")
+	
+	floors_file.close()
 
 
 # screen title and size
@@ -26,7 +74,6 @@ entry_box.pack(side = TOP)
 
 # Search Button
 search_button = tk.Button(screen, text="Search", bg="Blue",command=search)
-search_button.pack()
 search_button.place(x = 550, y = 20)
 
 img = ImageTk.PhotoImage(Image.open("campus.png"))
