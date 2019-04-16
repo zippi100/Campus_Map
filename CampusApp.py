b36ba263203b864ff70b99553e04c8fa86fbaf01
Ye-Rham <43466234+Ye-Rham@users.noreply.github.com>
2019-04-15 21:34:43 -0700

    A lot of things + info box prep

--- CampusApp.py
+++ CampusApp.py
@@ -3,63 +3,56 @@ from tkinter import *
 from PIL import ImageTk, Image
 import os
 
+
 screen = tk.Tk()
+canvas = tk.Canvas(screen)
+canvas.pack(fill=tk.BOTH, expand=1)
+floors_file = open("Floors.txt", "r")
+floors = []
+for line in floors_file:
+	floors.append(line.rstrip())
+floors_file.close()
 
 
 def search():
-	floors_file = open("Floors.txt", "r")
-	flag = 0
-	start = 0
-	searched = str(entry_box.get())
-	last_position = floors_file.tell()
-	
-	# print (floors_file.readline())
-	"""for line in floors_file.readline():
-		print ("test 1")
-		print (line)
-		for word in line.split():
-			print ("test 2")
-			print (word)
-			if word == searched:
-				print ("test 3")
-				flag = 1
-				while word != ',':
-	
-	
-				print (line)"""
-	if "100" > "101":
-		print("true")
-	
-	if searched == '':
-		pass
-	
-	elif (searched >= "0" and searched < "4000000000000") or searched < "0":
-		pass
-	
-	elif searched in floors_file.read():
-		
-		floors_file.seek(last_position)		
-		
-		for line in floors_file.readlines():
-			
-			if searched in line:			
-				start = 1
+	flag = False
+	floor_info = ""
 
-			if start == 0:
-				pass
-			else:
-				if "," in line:				
-					break
-				else:
-					print (line)
-		flag = 1
-	
-	if flag == 0:
-		not_valid = tk.Label(screen, text="Not A Valid Input, Search Again")
-		not_valid.place(x=625, y=25)
-		print("Not a valid input")
-	
-	floors_file.close()
+	if entry_box.get() is not "":
+		for x in range(0, len(floors)):
+			if selection.get() == floors[x].split('	', 1)[0]:
+				while floors[x] != "," and flag is not True:
+					x += 1
+					if entry_box.get() == floors[x]:
+						flag = True
+						not_valid.place_forget()
+						while len(floors[x].split(' ', 2)) != 2:
+							x -= 1
+						floor_info = floors[x].replace(':', '')
+
+		if not flag:
+			not_valid.place(x=645, y=25)
+	print(selection.get())
+	if selection.get() == "E":
+		if flag:
+			canvas.coords(info_box, 440, 240, 620, 380)
+			info_text.config(text=entry_box.get() + " is on the " + floor_info)
+			info_text.place(x=456, y=241)
+	elif selection.get() == "CS":
+		if flag:
+			canvas.coords(info_box, 470, 270, 650, 410)
+			info_text.config(text=entry_box.get() + " is on the " + floor_info)
+			info_text.place(x=486, y=271)
+	elif selection.get() == "EC":
+		print("True")
+	elif selection.get() == "GH":
+		print("")
+	elif selection.get() == "Hum":
+		print("")
+	elif selection.get() == "LH":
+		print("")
+	elif selection.get() == "MH":
+		print("")
 
 
 # screen title and size
@@ -68,20 +61,38 @@ screen.geometry("930x610")
 
 # What can be searched for
 title_question = tk.Label(screen, text="Classes to Search:")
-title_question.pack(side = TOP)
+title_question.place(x=401, y=0)
 
+# Campus image
+campus_map = tk.PhotoImage(file="campus.png")
+img = canvas.create_image(0, 100, anchor=tk.NW, image=campus_map)
 
 # Entry Box
-entry_box = tk.Entry(screen)
-entry_box.pack(side = TOP)
+Label(screen, text="Room #:").place(x=438, y=25)
+entry_box = tk.Entry(screen, width=6)
+entry_box.place(x=500, y=25)
 
 # Search Button
-search_button = tk.Button(screen, text="Search", bg="Blue",command=search)
-search_button.place(x = 550, y = 20)
+search_button = tk.Button(screen, text="Search", bg="Blue", command=search)
+search_button.place(x=558, y=20)
 
-img = ImageTk.PhotoImage(Image.open("campus.png"))
-panel = tk.Label(screen, image = img)
-panel.pack(side = BOTTOM)
+# Dropdown menu
+Label(screen, text="Building Initials:").place(x=252, y=25)
+buildings = ["E", "CS", "EC", "GH", "Hum", "LH", "MH"]
+selection = StringVar(screen)
+selection.set(buildings[0])
+buildings_dropdown = OptionMenu(screen, selection, *buildings)
+buildings_dropdown.config(width=3)
+buildings_dropdown.place(x=360, y=20)
 
+# Information box
+info_box = canvas.create_rectangle(0, 0, 0, 0, fill="#adadad")
+info_text = Label(screen, bg="#adadad")
+
+# Building highlight
+
+
+# Error text
+not_valid = tk.Label(screen, text="Invalid room #")
 
 screen.mainloop()
